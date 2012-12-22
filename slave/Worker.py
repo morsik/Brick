c743dc498e760db2615c300e17cd55e1bc20fefb
morsik <morsik@darkserver.it>
2012-12-22 16:35:47 +0100

    Added remote commands execution

--- slave/Worker.py
+++ slave/Worker.py
@@ -1,4 +1,5 @@
-from time import sleep
+import sys
+import time
 
 _working = False
 
@@ -15,16 +16,37 @@ def setWorking(isworking):
 	_working = isworking
 
 
-def doTask(cmd):
+# check this:
+# http://sharats.me/the-ever-useful-and-neat-subprocess-module.html#watching-both-stdout-and-stderr
+#
+def doTask(cmdline):
+	print("cmdline: \033[0;33m%s\033[0m" % cmdline)
 	setWorking(True)
 
-	i = 0
-	import random
-	slmax = random.randint(1, 10)
-	while i <= slmax:
-		print("Sleep %i/%i..." % (i, slmax))
-		sleep(1)
-		i += 1
+	import subprocess, shlex
+
+	out = ""
+	ts_start = None
+	try:
+		p = subprocess.Popen(shlex.split(str(cmdline)), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		ts_start = time.time()
+		while p.poll() is None:
+			ln = p.stdout.readline()
+			printts(ts_start, ln)
+			#out += ln
+
+		print("Total command time: %.8fs" % (time.time() - ts_start))
+	except OSError as msg:
+		aftertime = ""
+		if ts_start:
+			aftertime = " after %.8fs" % (time.time() - ts_start)
+
+		print("\033[1;31mCommand failed%s\033[0m" % (aftertime))
+		print("\033[1;37mCommand:\033[0m %s" % (cmdline))
 
 	setWorking(False)
 	return True
+
+
+def printts(ts_start, msg):
+	print("[%12.4f] %s" % (time.time() - ts_start, msg.strip()))
